3b496f6d50fe00594ee7d403fef61286616b07cc
Shimaa Adel <shimaa@tera-cit.com>
2021-12-14 12:29:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div>
+    <header class="bg-white shadow" v-if="$route.meta.title">
+      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <h1
+         
+          class="text-3xl font-bold leading-tight text-gray-900"
+        >
+          {{ $route.meta.title }} 
+        </h1>
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
